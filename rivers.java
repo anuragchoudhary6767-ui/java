3e28f1f00ea4212a28725aab97b535bb22eaedee
Anurag choudhary <anurag.choudhary6767@gmail.com>
2026-04-08 11:05:05 +0530

    Add files via upload

---
+++ rivers.java
@@ -0,0 +1,19 @@
+     public class rivers{
+     
+     
+    public static void main(String[] args){
+     String Str1="anurag";
+     String rev="";
+
+    
+     for ( int i=Str1.length()-1;i>=0;i--){
+          rev = rev+Str1.charAt(i);
+
+     }
+     System.out.println(rev);
+
+
+
+     
+     }
+}     
