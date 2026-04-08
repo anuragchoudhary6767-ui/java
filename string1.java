918353d82f3c12aa1b52b93d4d32a434336efe2c
Anurag choudhary <anurag.choudhary6767@gmail.com>
2026-04-08 11:03:54 +0530

    Add files via upload

---
+++ string1.java
@@ -0,0 +1,19 @@
+public class string1 {
+     public static void main(String[] args){
+         String Str1= "hii";
+         String Str2="hell";
+
+         
+         System.out.println(Str1);
+         System.out.println(Str2);
+         System.out.println(Str1);
+         System.out.println(Str1.length());
+         System.out.println(Str1.substring(0,2));
+         System.out.println(Str1.replace('1','z'));
+         System.out.println(Str1.toUpperCase());
+         System.out.println(Str1.toLowerCase());
+         System.out.println(Str1);
+
+         }
+    
+}
