dc34b06eaabe9f549fb496d64ff940cfcb91b5c4
Anurag choudhary <anurag.choudhary6767@gmail.com>
2026-04-10 11:16:11 +0530

    Add files via upload

---
+++ recursion.java
@@ -0,0 +1,18 @@
+public class recursion {
+    public static void  main(String[]args){
+        String str1 ="abc";
+        int count =0;
+        for (int i=0; i<str1.length();i++){
+            for(int j=i;j<str1.length();j++){
+                if(str1.charAt(i)== str1.charAt(j)){
+                    count++;
+                }
+            }
+        }
+        System.out.println(count);
+
+    }
+    
+}
+
+
