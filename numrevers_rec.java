4a83001e5ef7b412744b525f94da93e029c95b82
Anurag choudhary <anurag.choudhary6767@gmail.com>
2026-04-17 21:36:27 +0530

    Add files via upload

---
+++ numrevers_rec.java
@@ -0,0 +1,18 @@
+public class numrevers_rec {
+    public static void main(String[] args ){
+        int num=12345;
+        System.out.println(reverse(num));
+    }
+
+     static int reverse (int num){
+        int rev=0;
+        if (num==0){
+            return rev;
+        }
+        rev=rev*10+num%10;
+        return reverse(num/10)*10+num%10;
+     }
+
+}
+    
+
