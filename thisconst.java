7e4eec817a4797b35cc61e0a86502b4fffdffbda
Anurag choudhary <anurag.choudhary6767@gmail.com>
2026-06-29 15:12:33 +0530

    Add files via upload

---
+++ thisconst.java
@@ -0,0 +1,15 @@
+class car
+{
+     public car(){
+         this(5);
+         System.out.println("hello");
+     }
+     public car(int a){
+        this(5,6);
+         System.out.println(a);
+     }
+     public car(int a,int b){
+         System.out.println("a"+" "+a+" b"+" "+b);
+
+    }
+}
